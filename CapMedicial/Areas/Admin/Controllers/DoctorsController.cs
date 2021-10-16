a812129df84514b3857e70407247bdd005ba879e
tranminhhi <tranminhhi2732000@gmail.com>
2021-10-16 21:58:01 +0700

    ImportExcel

--- CapMedicial/Areas/Admin/Controllers/DoctorsController.cs
+++ CapMedicial/Areas/Admin/Controllers/DoctorsController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using CapMedicial.Models;
+using ExcelDataReader;
 
 namespace CapMedicial.Areas.Admin.Controllers
 {
@@ -17,7 +19,103 @@ namespace CapMedicial.Areas.Admin.Controllers
         // GET: Admin/Doctors
         public ActionResult Index()
         {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                dt = (DataTable)Session["tmpdata"];
+            }
+            catch (Exception ex)
+            {
+
+            }
+            ViewBag.DT = dt;
             return View(db.Doctors.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ImportExcel(HttpPostedFileBase[] uploads)
+        {
+            DataTable dt = new DataTable();
+            IExcelDataReader reader = null;
+            DataSet result = new DataSet();
+            for(int k = 0; k < uploads.Length; k++)
+            {
+                if (ModelState.IsValid)
+            {
+
+                if (uploads[k] != null && uploads[k].ContentLength > 0)
+                {
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    Stream stream = uploads[k].InputStream;
+
+                    
+
+
+                    if (uploads[k].FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (uploads[k].FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                    int fieldcount = reader.FieldCount;
+                    int rowcount = reader.RowCount;
+                    DataRow row;
+                    DataTable dt_ = new DataTable();
+                    try
+                    {
+                        dt_ = reader.AsDataSet().Tables[0];
+                        if (dt.Columns.Count == 0)
+                        {
+                            for (int i = 0; i < dt_.Columns.Count; i++)
+                            {
+                                dt.Columns.Add(dt_.Rows[0][i].ToString());
+                            }
+                        }
+                        int rowcounter = 0;
+                        for (int row_ = 1; row_ < dt_.Rows.Count; row_++)
+                        {
+                            row = dt.NewRow();
+
+                            for (int col = 0; col < dt_.Columns.Count; col++)
+                            {
+                                row[col] = dt_.Rows[row_][col].ToString();
+                                rowcounter++;
+                            }
+                            dt.Rows.Add(row);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("File", "Unable to Upload file!");
+                        return RedirectToAction("Index");
+                        }
+
+                    
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            }
+            
+            result.Tables.Add(dt);
+            reader.Close();
+            reader.Dispose();
+            DataTable tmp = result.Tables[0];
+            Session["tmpdata"] = tmp;  //store datatable into session
+            return RedirectToAction("Index");
         }
 
         // GET: Admin/Doctors/Details/5
@@ -46,7 +144,7 @@ namespace CapMedicial.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Description,Department")] Doctor doctor)
+        public ActionResult Create(Doctor doctor)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +176,7 @@ namespace CapMedicial.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Description,Department")] Doctor doctor)
+        public ActionResult Edit(Doctor doctor)
         {
             if (ModelState.IsValid)
             {
